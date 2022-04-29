29377426bc3d3e27ddc184508e40cdc378ac95bd
daxianyu <daxianyu@outlook.com>
2022-04-29 22:55:42 +0800

    add request

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
-import { version } from "antd";
+import { Axios } from 'axios';
 import { Input } from 'antd'
 import "antd/dist/antd.css";
 import "./index.css";
+
+const request = new Axios({
+  baseURL: 'https://iyao.daxianyu.cn',
+});
+
+request.get('/query').then(res => console.log(res))
 
 ReactDOM.render(
   <div className="App">
     <h1>雪球文章搜索</h1>
     <Input />
+    <List />
   </div>,
   document.getElementById("root")
 );
+
+function List() {
+  useEffect(() => {
+  }, [])
+
+  return <div>12313</div>;
+}
