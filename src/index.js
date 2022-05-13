a6381c027520df95e1e3c2cc605055aa9a725a45
daxianyu <daxianyu@outlook.com>
2022-05-13 14:37:57 +0800

    订阅和反订阅

--- src/index.js
+++ src/index.js
@@ -27,13 +27,14 @@ ReactDOM.render(
 
 function List() {
   const l = useRef([]);
+  const [userMap, setUserMap] = useState({});
   const [page, setPage] = useState(1);
   const [tempPage, setTempPage] = useState(1);
   const [list, setList] = useState([]);
-  const [userList, setUserList] = useState([]);
   const [user, setUser] = useState("");
   const [toSub, setToSub] = useState(null);
   const [loginVisible, setLoginVisibility] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
   function handleChangeUser(userId) {
     setPage(1);
     setTempPage(1);
@@ -43,8 +44,23 @@ function List() {
     try {
       const response = await request.get("/userlist");
       const users = JSON.parse(response.data);
-      setUserList(users);
-      setUser(users.length ? users[0].id : "");
+      const map = {};
+      users.forEach((u) => {
+        map[u.id] = u;
+      });
+      setUserMap(map);
+    } catch (err) {
+      console.error(JSON.stringify(err));
+    }
+  }
+
+  async function fetchCurrentUser() {
+    try {
+      const response = await request.get("/userInfo");
+      const userInfo = JSON.parse(response.data);
+      userInfo.subs = userInfo.subs || [];
+      setCurrentUser(userInfo);
+      setUser(userInfo.subs.length ? userInfo.subs[0] : "");
     } catch (err) {
       console.error(JSON.stringify(err));
     }
@@ -58,11 +74,12 @@ function List() {
       return res;
     });
     fetchUserList();
+    fetchCurrentUser();
   }, []);
 
   function isSubscribed(sub) {
     if (!sub) return false;
-    return userList.some((u) => u.id === +sub.value);
+    return currentUser.subs.some((u) => u.id === +sub.value);
   }
 
   function subscribe(subId, name) {
@@ -73,6 +90,7 @@ function List() {
           JSON.stringify({ id: subId, name: name })
         );
         await fetchUserList();
+        await fetchCurrentUser();
       } catch (err) {
         message.error(err.message);
       }
@@ -85,11 +103,36 @@ function List() {
       try {
         await request.delete("/unsubscribe?id=" + subId);
         await fetchUserList();
+        await fetchCurrentUser();
       } catch (err) {
         message.error(err.message);
       }
     }
     unSubscribeFetch();
+  }
+
+  function listen(subId) {
+    async function listenFetch() {
+      try {
+        await request.post(`/listen`, JSON.stringify({ id: subId }));
+        await fetchCurrentUser();
+      } catch (err) {
+        message.error(err.message);
+      }
+    }
+    listenFetch();
+  }
+
+  function unListen(subId) {
+    async function unListenFetch() {
+      try {
+        await request.delete("/unlisten?id=" + subId);
+        await fetchCurrentUser();
+      } catch (err) {
+        message.error(err.message);
+      }
+    }
+    unListenFetch();
   }
 
   useEffect(() => {
@@ -119,41 +162,72 @@ function List() {
   return (
     <div>
       <div>
-        <Select value={user} onChange={handleChangeUser}>
-          {userList.map((u) => (
-            <Select.Option value={u.id} key={u.name}>
-              {u.name}
-            </Select.Option>
-          ))}
-        </Select>
-
-        <SearchSelect
-          style={{ width: 150, marginLeft: 10 }}
-          value={toSub}
-          onChange={(sub) => {
-            setToSub(sub);
-          }}
-        />
-        {toSub ? (
-          isSubscribed(toSub) ? (
+        {(currentUser && currentUser.subs.length && (
+          <Select
+            style={{ width: 150, marginRight: 10 }}
+            value={user}
+            onChange={handleChangeUser}
+          >
+            {currentUser &&
+              currentUser.subs.map((subId) => {
+                const u = userMap[subId];
+                if (!u) return null;
+                return (
+                  <Select.Option value={u.id} key={u.name}>
+                    {u.name}
+                  </Select.Option>
+                );
+              })}
+          </Select>
+        )) ||
+          null}
+        {user ? (
+          currentUser.listen && currentUser.listen.indexOf(+user) > -1 ? (
             <Button
               onClick={() => {
-                unSubscribe(toSub.value);
+                unListen(user);
               }}
             >
-              取消监听
+              取消推送
             </Button>
           ) : (
             <Button
               onClick={() => {
-                subscribe(toSub.value, toSub.text);
+                listen(user);
               }}
             >
-              监听
+              推送
             </Button>
           )
         ) : null}
-        {toSub ? <Button>订阅</Button> : null}
+        <div style={{ marginTop: 10 }}>
+          <SearchSelect
+            style={{ width: 150, marginRight: 10 }}
+            value={toSub}
+            onChange={(sub) => {
+              setToSub(sub);
+            }}
+          />
+          {toSub ? (
+            isSubscribed(toSub) ? (
+              <Button
+                onClick={() => {
+                  unSubscribe(toSub.value);
+                }}
+              >
+                取消监听
+              </Button>
+            ) : (
+              <Button
+                onClick={() => {
+                  subscribe(toSub.value, toSub.text);
+                }}
+              >
+                监听
+              </Button>
+            )
+          ) : null}
+        </div>
       </div>
 
       {list.map((item) => {
